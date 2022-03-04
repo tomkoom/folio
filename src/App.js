eec1fd63a8bba4b91c81acf7bd66e7db36adb70c
tomkoom <tomash.sugint@gmail.com>
2022-03-04 21:38:40 +0300

    feat: center link on the screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import './App.css';
-import About from './Components/About/About';
-import Nav from './Components/Nav/Nav';
-import Skills from './Components/Skills/Skills';
-import Projects from './Components/Projects/Projects';
-import Footer from './Components/Footer/Footer';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <a href="https://bit.ly/33sPgEW" rel="norefferer noopener">Public journal <span style={{ fontSize: "0.9rem" }}><FontAwesomeIcon icon={faArrowRight} /></span></a>
-      {/* <Nav />
-      <About />
-      <Skills />
-      <Projects />
-      <Footer /> */}
     </div>
   );
 }
