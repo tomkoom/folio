b4645a4d4c1922de1826ff17aeac11cdad2b5020
tomkoom <tomash.sugint@gmail.com>
2026-02-02 00:04:58 +0700

    update constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = "tmkm / cr4ft 🛠️";
+export const APP_NAME = "tmkm / cr4ft";
 export const APP_DESCRIPTION = "";
 export const MODE = process.env.NODE_ENV;
 export const PROD_URL = "";
